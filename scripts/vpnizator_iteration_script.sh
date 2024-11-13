b72c2a4fa2353560835a7ffabe21f7014de263d1
Ardgellan <aegiev01@gmail.com>
2024-11-13 13:50:55 +0300

    iteration_upgrades_2

--- scripts/vpnizator_iteration_script.sh
+++ scripts/vpnizator_iteration_script.sh
@@ -7,6 +7,9 @@ cp VPNizator/source/data/.env /root/
 rm -rf VPNizator
 
 git clone https://github.com/Ardgellan/VPNizator.git
+cd VPNizator
+git switch develop
+cd ~
 
 mv .env VPNizator/source/data/
 
