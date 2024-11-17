82fed5d40b0e00153c74c6f824fb6992fdf9a2dd
Ardgellan <aegiev01@gmail.com>
2024-11-17 16:18:24 +0300

    iteration_upgrades_7

--- scripts/ufw_setup.sh
+++ scripts/ufw_setup.sh
@@ -10,44 +10,44 @@ if [[ "$confirm" != "yes" ]]; then
     exit 1
 fi
 
-# Проверяем, установлен ли ufw
-if ! command -v ufw &> /dev/null; then
-    echo -e "\033[31mUFW не установлен. Устанавливаю UFW...\033[0m"
-    sudo apt update && sudo apt install ufw -y
-    if ! command -v ufw &> /dev/null; then
-        echo -e "\033[31mОшибка! Не удалось установить UFW.\033[0m"
-        exit 1
-    fi
-    echo -e "\033[32mUFW успешно установлен.\033[0m"
-else
-    echo -e "\033[32mUFW уже установлен.\033[0m"
-fi
+# # Проверяем, установлен ли ufw
+# if ! command -v ufw &> /dev/null; then
+#     echo -e "\033[31mUFW не установлен. Устанавливаю UFW...\033[0m"
+#     sudo apt update && sudo apt install ufw -y
+#     if ! command -v ufw &> /dev/null; then
+#         echo -e "\033[31mОшибка! Не удалось установить UFW.\033[0m"
+#         exit 1
+#     fi
+#     echo -e "\033[32mUFW успешно установлен.\033[0m"
+# else
+#     echo -e "\033[32mUFW уже установлен.\033[0m"
+# fi
 
-# IP-адрес Nginx-сервера (укажите свой)
-NGINX_IP="45.12.137.116"
+# # IP-адрес Nginx-сервера (укажите свой)
+# NGINX_IP="45.12.137.116"
 
-# IP-адрес вашего компьютера (укажите свой)
-MY_COMPUTER_IP="95.164.8.66"
+# # IP-адрес вашего компьютера (укажите свой)
+# MY_COMPUTER_IP="95.164.8.66"
 
-# Разрешить доступ с IP Nginx-сервера
-echo "Разрешаем доступ с IP Nginx-сервера: $NGINX_IP"
-sudo ufw allow from "$NGINX_IP" to any port 80
-sudo ufw allow from "$NGINX_IP" to any port 443
+# # Разрешить доступ с IP Nginx-сервера
+# echo "Разрешаем доступ с IP Nginx-сервера: $NGINX_IP"
+# sudo ufw allow from "$NGINX_IP" to any port 80
+# sudo ufw allow from "$NGINX_IP" to any port 443
 
-# Разрешить доступ с вашего компьютера
-echo "Разрешаем доступ с вашего компьютера: $MY_COMPUTER_IP"
-sudo ufw allow from "$MY_COMPUTER_IP" to any port 80
-sudo ufw allow from "$MY_COMPUTER_IP" to any port 443
+# # Разрешить доступ с вашего компьютера
+# echo "Разрешаем доступ с вашего компьютера: $MY_COMPUTER_IP"
+# sudo ufw allow from "$MY_COMPUTER_IP" to any port 80
+# sudo ufw allow from "$MY_COMPUTER_IP" to any port 443
 
-# Заблокировать доступ ко всем остальным
-echo "Блокируем доступ ко всем остальным IP-адресам на порты 80 и 443"
-sudo ufw deny 80
-sudo ufw deny 443
+# # Заблокировать доступ ко всем остальным
+# echo "Блокируем доступ ко всем остальным IP-адресам на порты 80 и 443"
+# sudo ufw deny 80
+# sudo ufw deny 443
 
-# Включаем ufw, если он еще не включен
-echo "Включаем UFW..."
-sudo ufw enable
+# # Включаем ufw, если он еще не включен
+# echo "Включаем UFW..."
+# sudo ufw enable
 
-# Проверка статуса UFW
-echo "Статус UFW:"
-sudo ufw status verbose
+# # Проверка статуса UFW
+# echo "Статус UFW:"
+# sudo ufw status verbose
